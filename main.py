da8750c16275c743ad24c0c7c2fe656803272221
ZYengr <86040087+ZYengr@users.noreply.github.com>
2022-08-17 21:07:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import cv2
-import infer
 
-violence_predictor = infer.ViolencePredictor()
 i = 0
-cap = cv2.VideoCapture("datasets/videos/Violence/V_129.mp4")
+cap = cv2.VideoCapture("dataset/Violence/V_129.mp4")
 # cap = cv2.VideoCapture("datasets/videos/NonViolence/NV_129.mp4")
-while i < 90:
+
+model_name_list = ['violenceModel_baseOn_inceptionV3.h5','violenceModel_baseOn_MobileNetV2.h5']
+vp_baseOn_inceptionV3 = ViolencePredictor(model_name = model_name_list[0], video_output_path='./infer_V_129_baseOn_inceptionV3.avi')
+vp_baseOn_MobileNetV2 = ViolencePredictor(model_name = model_name_list[1], video_output_path='./infer_V_129_baseOn_MobileNetV2.avi')
+
+while i < 180:
     i += 1
-    _, img = cap.read()
-    violence = violence_predictor(img)
-    print(violence)
+    grabbed, img = cap.read()
+    if not grabbed:
+	    break
+    violence_1 = vp_baseOn_inceptionV3.process(img)
+    violence_2 = vp_baseOn_MobileNetV2.process(img)
+    print(violence_1,violence_2)
